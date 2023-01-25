7680e7bd58884938dfcc0ade0aab88adae2e11fd
burgues0 <victordpp@outlook.com>
2023-01-25 16:59:44 +0044

    feat: nature command complete & add command description (use --help)

--- pykedex/cli.py
+++ pykedex/cli.py
@@ -2,15 +2,14 @@
 
 import asyncio, aiopoke, time
 import typer, json
+from rich.console import Console
+from rich.table import Table
 from pathlib import Path
 from typing import Optional
 from pykedex import __app_name__, __version__
-# from rich import print
-
-
-
 
 app = typer.Typer()
+appConsole = Console()
 PROJECT_DIR = Path().resolve()
 JSON_CONTENT_DIR = PROJECT_DIR / "data"
 
@@ -35,45 +34,54 @@ def main(
     return
 
 @app.command()
-def pokedex(
-
-) -> None:
+def pokedex() -> None:
+    """
+    Displays all of the avaliable Pokémon in the National Dex order (all 1008).
+    """    
     return
 
 @app.command()
 def moves() -> None:
+    """
+    Displays all of the avaliable Pokémon moves.
+    """    
     print("ok")
 
 @app.command()
-def abilities(
-
-) -> None:
+def abilities() -> None:
+    """
+    Displays all of the avaliable Pokémon abilities.
+    """    
     return
 
 @app.command()
-def items(
-
-) -> None:
+def items() -> None:
+    """
+    Displays all of the avaliable usable items in the digital franchise.
+    """    
     return
 
 @app.command()
-def typechart(
-
-) -> None:
+def typechart() -> None:
+    """
+    Displays the Type Effectiveness Chart.
+    """
     return
 
 @app.command()
 def nature():
+    """
+    Displays all of the avaliable Pokémon natures.
+    """
     file = json.load(open(JSON_CONTENT_DIR / "natures.json"))
+    natureTable = Table(title="Pokémon Natures")
+    natureTable.add_column("Nature")
+    natureTable.add_column("Increases", style="green")
+    natureTable.add_column("Decreases", style="red")
     for nature in file:
-        print(nature)
+        natureTable.add_row(str(nature).capitalize(), str(file[nature]['increased_stat']), str(file[nature]['decreased_stat']))
+    appConsole.print(natureTable)
 
 @app.command()
-def tools(
-
-) -> None:
+def tools() -> None:
     return
-
-@app.command()
-def create(username: str):
-    print(f"Creating user: {username}")
